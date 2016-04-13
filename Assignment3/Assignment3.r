e8c6d549f0dd5caea4ae7a08752aa2a091b80d12
Kevin Ku <kkuo12887@gmail.com>
2016-04-13 10:13:51 -0400

    Adding missing files.

--- Assignment3/Assignment3.r
+++ Assignment3/Assignment3.r
@@ -11,6 +11,30 @@ library(cluster)
 library(fpc)
 
 
+# Summarize Class Output
+sum.class <- summary(habermansurvival$survival_status)
+sum.class
+
+
+# Summarize age
+sum.age <- summary(habermansurvival$age)
+sum.age
+
+
+# Summarize operation year
+sum.operation_year <- summary(habermansurvival$operation_year)
+sum.operation_year
+
+
+# Summarize axillary nodes
+sum.positive_axillary_nodes <- summary(habermansurvival$positive_axillary_nodes)
+sum.positive_axillary_nodes
+
+
+###############################################################################
+#k-means
+###############################################################################
+
 #plot the toy dataset
 plot(m)
 #run k-means with k = 2
@@ -34,23 +58,96 @@ plot(m, col=km3$cluster)
 points(km3$centers,pch='x',cex=1.5)
 
 
+###############################################################################
+#evaluating k-means in R
+###############################################################################
+#create distance matrix for cluster.stats
+distm <- dist(m)  
+#calculate cluster statistics for km2
+cstatskm2 = cluster.stats(distm,km2$cluster)
+#calculate cluster statistics for km3
+cstatskm3 = cluster.stats(distm,km3$cluster) 
+#evaluate between and within cluster distances
+cstatskm2$average.between
+cstatskm2$average.within
+cstatskm3$average.between
+cstatskm3$average.within
 
-# Summarize Class Output
-sum.class <- summary(habermansurvival$survival_status)
-sum.class
 
 
-# Summarize age
-sum.age <- summary(habermansurvival$age)
-sum.age
+#kmedoids example
+distm <- dist(m) 
+#run PAM with k = 2
+pam2 <- pam(m, 2)
+
+#visualize results colored by cluster
+plot(m, col=pam2$cluster)
+
+#plot cluster centers
+points(pam2$medoids,pch='O',cex=1.5)
+
+#run PAM with k = 3
+pam3 <- pam(m, 3)
+
+#visualize results colored by cluster
+plot(m, col=pam3$cluster)
+
+#plot cluster centers
+points(pam3$medoids,pch='O',cex=1.5)
+
+#evaluate PAM
+cstatspam2 <- cluster.stats(distm,pam2$cluster)
+cstatspam3 <- cluster.stats(distm,pam3$cluster)
 
 
-# Summarize operation year
-sum.operation_year <- summary(habermansurvival$operation_year)
-sum.operation_year
+#evaluate between and within cluster distances
+cstatskm2$average.between
+cstatskm2$average.within
+cstatskm3$average.between
+cstatskm3$average.within
 
 
-# Summarize axillary nodes
-sum.positive_axillary_nodes <- summary(habermansurvival$positive_axillary_nodes)
-sum.positive_axillary_nodes
 
+#single linkage example
+agn <- agnes(m, diss=FALSE, stand=FALSE, method="single") 
+dend_agn <- as.dendrogram(agn)
+plot(dend_agn, xlab="Index of Data Points", ylab = "Steps", main = "Single-Linkage Clustering")
+
+#complete linkage example
+agn <- agnes(m, diss=FALSE, stand=FALSE, method="complete") 
+dend_agn <- as.dendrogram(agn)
+plot(dend_agn, xlab="Index of Data Points", ylab = "Steps", main = "Complete-Linkage Clustering")
+
+
+
+
+plot(m)
+#Run dbscan with eps = 5 and MinPts = 5
+dbr <- dbscan(m, eps=5, MinPts=5)
+str(dbr)
+plot(m, col=dbr$cluster+1L)
+
+#Run dbscan with eps = 7 and MinPts = 5
+dbr <- dbscan(m, eps=7, MinPts=5)
+str(dbr)
+plot(m, col=dbr$cluster+1L)
+
+#Run dbscan with eps = 10 and MinPts = 5
+dbr <- dbscan(m, eps=10, MinPts=5)
+str(dbr)
+plot(m, col=dbr$cluster+1L)
+
+#Run dbscan with eps = 20 and MinPts = 5 
+dbr <- dbscan(m, eps=20, MinPts=5)
+str(dbr)
+plot(m, col=dbr$cluster+1L)
+
+#Run dbscan with eps = 50 and MinPts = 5 
+dbr <- dbscan(m, eps=50, MinPts=5)
+str(dbr)
+plot(m, col=dbr$cluster+1L)
+
+# Silhouette plot
+d <- dist(m)
+sil <- silhouette(dbr$cluster,d)
+plot(sil)
